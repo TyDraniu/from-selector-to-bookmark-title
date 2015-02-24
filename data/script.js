d6bfe996bffb8856ff7ae14c75a50c9bed00c7e0
Krzysztof Głębowicz <krzysztof.glebowicz@it-pkp.pl>
2015-02-24 11:17:50 +0100

    working code

---
+++ data/script.js
@@ -0,0 +1,21 @@
+﻿function getPostNumber()
+{
+try {
+	var selector = document.querySelector('#page-body > P:last-child > STRONG:first-child');
+
+	if (selector != null) {
+		console.log("gpn(): " + selector.innerHTML);
+		return selector.innerHTML;
+	}
+	else {
+		console.exception("Brak selektora");
+		return null;
+	}
+}
+catch (e) {
+	console.exception(e.message);
+	return null;
+	}
+};
+
+self.postMessage(getPostNumber(), "*");
---
+++ index.js
@@ -0,0 +1,36 @@
+var self = require('sdk/self');
+var data = require("sdk/self").data;
+var { setInterval } = require("sdk/timers");
+
+
+setInterval(function () {
+	let pageWorker = require("sdk/page-worker").Page({
+	  contentScriptFile: data.url("script.js"),
+	  contentURL: "http://mozillapl.org/forum/index.php",
+	  contentScriptWhen: "ready"
+	  });
+	  
+	pageWorker.on("message", function(message) {
+		let { search, UNSORTED, save } = require("sdk/places/bookmarks");
+		
+	//query places/bookmarks
+		search({ query: "mozillapl.org" }, { sort: "title" }).on("end", bookmarkList );
+		
+		function bookmarkList(bookmarks) {
+			for (i in bookmarks) {
+			  console.log("bookmark: " + bookmarks[i].title);
+			  bookmarks[i].title = message;
+			};
+			save(bookmarks).on("data", function (item, inputItem) {
+			  // Each item in `bookmarks` has its own `data` event
+			}).on("end", function (results, inputResults) {
+			  // `results` is an array of items saved in the same order
+			  // as they were passed in.
+			});
+		};
+	});
+	
+}, 60000);
+	  
+
+
